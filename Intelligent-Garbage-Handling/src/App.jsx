4a7b48950b4fc9a9375de2d8b773d57c987460f5
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-02-11 19:02:28 +0530

    routing structure done

--- Intelligent-Garbage-Handling/src/App.jsx
+++ Intelligent-Garbage-Handling/src/App.jsx
@@ -1,9 +1,23 @@
 import { Button } from "@/components/ui/button"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import DashboardPage from "@/pages/dashboardpage";
+import AboutPage from "@/pages/aboutpage";
+import LandingPage from "@/pages/landingpage";
+import MapPage from "@/pages/mappage.jsx";
+
 
 export default function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/dashboard" element={<DashboardPage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/map" element={<MapPage/>}/>
+        </Routes>
+        </Router>
+    </>
   )
 }
