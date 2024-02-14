7311f3e2eeb3dc0a09eae1857e1d43483866eb95
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-02-14 20:12:05 +0530

    done folders

---
+++ garbage-handling/src/App.jsx
@@ -0,0 +1,27 @@
+import LandingPage from "./pages/landingpage"
+import AboutPage from "./pages/aboutpage"
+import DashboardPage from "./pages/dashboardpage"
+import MapPage from "./pages/mappage"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </Router>
+
+
+      
+    </>
+  )
+}
+
+export default App
