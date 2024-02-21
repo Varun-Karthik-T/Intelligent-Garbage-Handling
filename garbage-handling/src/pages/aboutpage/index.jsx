a4a840d5004eab8a458f0f4fcb54a65cfebecb53
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-02-21 18:27:53 +0530

    image imported

--- garbage-handling/src/pages/aboutpage/index.jsx
+++ garbage-handling/src/pages/aboutpage/index.jsx
@@ -1,7 +1,20 @@
-export default function AboutPage(){
-    return(
-        <>
-        <p>about page</p>
-        </>
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+
+import aamir from "../../assets/MOHAMED_AAMIR_GHOUSE.jpg"
+import varun from ""
+
+export default function LandingPage() {
+    return (
+
+        <Avatar>
+            <AvatarImage src={aamir} alt="@shadcn" />
+            <AvatarFallback>MA</AvatarFallback>
+        </Avatar>
+
+
     )
 }
